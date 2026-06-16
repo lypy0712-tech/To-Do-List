a804b7ab7a76391a4acc3b62adaaf3b779b1255e
lypy0712-tech <lypy0712@gmail.com>
2026-06-16 08:48:01 +0400

    modified main.py added CRUD and Pydentic classes added

---
+++ main.py
@@ -0,0 +1,60 @@
+from uuid import uuid4
+
+from fastapi import FastAPI, HTTPException, status
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods = ["*"]
+)
+
+class TaskSchema(BaseModel):
+    id: str
+    title: str
+    completed: bool
+
+
+class TaskCreateSchema(BaseModel):
+    title: str
+
+
+class TaskUpdateSchema(BaseModel):
+    title: str | None= None
+    completed: bool | None= None
+
+tasks: list[TaskSchema] = []
+
+
+@app.get("/tasks")
+def read_tasks() -> list[TaskSchema]:
+    return tasks
+
+
+@app.post("/tasks", status_code= status.HTTP_201_CREATED)
+def create_task(payload: TaskCreateSchema)-> TaskSchema:
+    new_task = TaskSchema(id= str(uuid4()), title= payload.title, completed= False)
+    tasks.append(new_task)  
+    return new_task
+
+
+@app.patch("/tasks/{task_id}")
+def create_task(task_id: str, payload: TaskUpdateSchema)-> TaskSchema:
+    for task in tasks:
+        if task_id == task.id:
+            if payload.title:
+                task.title = payload.title
+            if payload.completed is not None: 
+                task.completed = payload.completed
+            return  task
+
+
+@app.delete("/tasks/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
+def cdelete_task(task_id: str):
+    for task in tasks:
+        if task_id == task.id:
+            tasks.remove(task)
+            return {"message" : f"Item {task_id} has been deleted"}
