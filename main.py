358a894c474f54297c256365bf773be8f0a28bcc
User <ramil0712@Server.azlogistics.net>
2026-07-04 18:45:26 +0400

    finished task backend 2

--- main.py
+++ main.py
@@ -1,10 +1,45 @@
+from contextlib import asynccontextmanager
 from uuid import uuid4
 
 from fastapi import FastAPI, HTTPException, status, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from sqlalchemy import create_engine, select, delete
+from sqlalchemy.orm import Mapped, mapped_column, sessionmaker, Session, DeclarativeBase
 
-app = FastAPI()
+DATABASE_URL = "postgresql+psycopg://postgres:admin@192.168.0.1:15432/postgres"
+engine = create_engine(DATABASE_URL)
+Sessionlocal= sessionmaker[Session](bind=engine)
+
+def get_db():
+    db = Sessionlocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+class Base(DeclarativeBase):
+    id: Mapped[str]= mapped_column(primary_key=True, default=lambda: str(uuid4()))
+
+
+class TaskORM(Base):
+    __tablename__= "tasks"
+
+    title: Mapped[str]
+    completed: Mapped[bool]= mapped_column(default=False)
+
+
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    Base.metadata.create_all(bind=engine)
+    yield
+
+
+app = FastAPI(
+    lifespan=lifespan,
+    title= "To-Do-List"
+    )
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -17,44 +52,60 @@ class TaskSchema(BaseModel):
     title: str
     completed: bool 
 
+    model_config={
+        "from_attributes":True
+    }
+
 
 class TaskCreateSchema(BaseModel):
     title: str
+
+    model_config={
+        "from_attributes":True
+    }
 
 
 class TaskUpdateSchema(BaseModel):
     title: str | None= None
     completed: bool | None= None
 
-tasks: list[TaskSchema] = []
+    model_config={
+        "from_attributes":True
+    }
+
+def task_orm_to_model(task_orm: TaskORM):
+    return TaskSchema(id= task_orm.id, title= task_orm.title, completed=task_orm.completed)
 
 
 @app.get("/tasks")
-def read_tasks() -> list[TaskSchema]:
-    return tasks
+def read_tasks(db: Session= Depends(get_db), response_model= TaskSchema) -> list[TaskSchema]:
+    task_from_db= db.scalars(select(TaskORM)).all()
+    return task_from_db
 
 
-@app.post("/tasks", status_code= status.HTTP_201_CREATED)
-def create_task(payload: TaskCreateSchema)-> TaskSchema:
-    new_task = TaskSchema(id= str(uuid4()), title= payload.title, completed= False)
-    tasks.append(new_task)  
+@app.post("/tasks", status_code= status.HTTP_200_OK)
+def create_task(payload: TaskCreateSchema, db: Session= Depends(get_db)) -> TaskSchema:
+    new_task = TaskORM(title= payload.title, completed= False)
+    db.add(new_task) 
+    db.commit()
     return new_task
 
 
-@app.patch("/tasks/{task_id}")
-def create_task(task_id: str, payload: TaskUpdateSchema)-> TaskSchema:
-    for task in tasks:
-        if task_id == task.id:
-            if payload.title:
-                task.title = payload.title
-            if payload.completed is not None: 
-                task.completed = payload.completed
-            return  task
+@app.patch("/tasks/{task_id}", status_code= status.HTTP_200_OK)
+def create_task(task_id: str, payload: TaskUpdateSchema, db: Session= Depends(get_db), response_model= TaskSchema)-> TaskSchema:
+    response = db.get(TaskORM, task_id)
+    if response:
+        if payload.title:
+            response.title = payload.title
+        if payload.completed is not None:
+            response.completed = payload.completed
+        db.commit()
+        return response
+    return HTTPException(status_code= 404, detail= f"Task with {task_id} not found")
 
 
 @app.delete("/tasks/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
-def cdelete_task(task_id: str):
-    for task in tasks:
-        if task_id == task.id:
-            tasks.remove(task)
-            return {"message" : f"Item {task_id} has been deleted"}
+def delete_task(task_id: str, db: Session= Depends(get_db)) -> None:
+    task_for_delete= db.get(TaskORM, task_id)
+    db.delete(task_for_delete)
+    db.commit()
